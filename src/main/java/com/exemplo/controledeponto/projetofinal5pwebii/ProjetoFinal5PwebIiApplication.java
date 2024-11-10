6af96505328c0f7687c904eda66e698a6636cd02
Bowie <diniz.rocha82@gmail.com>
2024-11-10 09:01:14 -0300

    ProjetoFinal5PwebIiApplication

---
+++ src/main/java/com/exemplo/controledeponto/projetofinal5pwebii/ProjetoFinal5PwebIiApplication.java
@@ -0,0 +1,13 @@
+package com.exemplo.controledeponto.projetofinal5pwebii;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoFinal5PwebIiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoFinal5PwebIiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/exemplo/controledeponto/projetofinal5pwebii/ProjetoFinal5PwebIiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exemplo.controledeponto.projetofinal5pwebii;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoFinal5PwebIiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
